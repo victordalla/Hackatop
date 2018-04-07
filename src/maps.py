740984c9acda43d6e4646ad4306bf6cf6090e5e2
Rodrigo Geurgas <bloodrodrigo@gmail.com>
2018-04-07 15:27:49 -0300

    ops

---
+++ src/maps.py
@@ -0,0 +1,16 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyA6KJ31khHds0Se0q36be_a00U0X4FCaew')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall", "Parramatta, NSW", mode="transit", departure_time=now)
+
+print(directions_result)
